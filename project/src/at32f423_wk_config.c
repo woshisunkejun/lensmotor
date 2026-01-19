8f5db47a17e29b7d27421cfb0eed22b72b6d048d
woshisunkejun <786659185@qq.com>
2026-01-19 11:59:37 +0800

    Enable USART1 interrupt in NVIC config

--- project/src/at32f423_wk_config.c
+++ project/src/at32f423_wk_config.c
@@ -228,6 +228,7 @@ void wk_nvic_config(void)
   NVIC_SetPriority(PendSV_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 0, 0));
   NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
   nvic_irq_enable(TMR2_GLOBAL_IRQn, 0, 0);
+  nvic_irq_enable(USART1_GLOBAL_IRQn, 0, 0);
 }
 
 /* add user code begin 1 */
